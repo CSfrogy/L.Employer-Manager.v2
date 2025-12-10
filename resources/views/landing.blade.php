d55780093263f007704c7d3e39bd15f8157829df
CSfrogy <lonelyguse@gmail.com>
2025-12-10 21:58:49 +0800

    load landing.css

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -13,9 +13,7 @@
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    @if(!file_exists(public_path('build/manifest.json')))
-        <link rel="stylesheet" href="{{ asset('css/landing.css') }}?v={{ filemtime(public_path('css/landing.css')) }}">
-    @endif
+    <link rel="stylesheet" href="{{ asset('css/landing.css') }}?v={{ filemtime(public_path('css/landing.css')) }}">
 </head>
 
 <body class="landing-page">
