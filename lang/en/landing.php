225ef67dc88226ed602343d087279ff5c5afc160
CSfrogy <lonelyguse@gmail.com>
2025-12-11 03:18:12 +0800

    cleaning

---
+++ lang/en/landing.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'title' => 'L.Employee Management System',
+    'description' => 'Employee Management System - Choose your login type',
+    'access_control' => 'Access Control',
+    'select_access' => 'Please select your access level to continue',
+    'employee_portal' => 'Enter Employee Portal',
+    'admin_panel' => 'Enter Admin Panel',
+    'system_name' => 'L.Employer Manager System',
+    'copyright' => '© 2025',
+];
---
+++ lang/zh-CN/landing.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'title' => 'L.员工管理系统',
+    'description' => '员工管理系统 - 选择您的登录类型',
+    'access_control' => '访问控制',
+    'select_access' => '请选择您的访问级别以继续',
+    'employee_portal' => '进入员工门户',
+    'admin_panel' => '进入管理面板',
+    'system_name' => 'L.雇主管理系统',
+    'copyright' => '© 2025',
+];
